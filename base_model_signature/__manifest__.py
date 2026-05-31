8d3a2a0798ea9aaab3087870f64aa338a26d6974
Agung Sepruloh <agungsepruloh1996@gmail.com>
2026-06-01 06:53:26 +0700

    feat(model_signature): upgrade to Odoo 19

--- base_model_signature/__manifest__.py
+++ base_model_signature/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Base Module Signature",
 
@@ -19,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/18.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Extra Tools',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
--- employee_signature/__manifest__.py
+++ employee_signature/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Employee Signature",
 
@@ -19,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/18.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human Resources/Employees',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr', 'base_model_signature'],
